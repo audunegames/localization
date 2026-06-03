2942cdbfee8635459c372a05d00e5e21d02e331b
Danae Dekker <hello@danaedekker.com>
2026-06-03 19:53:07 +0200

    Fixed documentation comments in LocalizationException

--- Runtime/LocalizationException.cs
+++ Runtime/LocalizationException.cs
@@ -8,13 +8,13 @@ namespace Audune.Localization
   public class LocalizationException : Exception
   {
     /// <summary>
-    /// Constructor for a message
+    /// Constructor for a message.
     /// </summary>
     /// <param name="message">The message of the exception.</param>
     public LocalizationException(string message) : base(message) { }
 
     /// <summary>
-    /// Constructor for a message and inner exception/
+    /// Constructor for a message and inner exception.
     /// </summary>
     /// <param name="message">The message of the exception.</param>
     /// <param name="innerException">The exception that caused this exception to be thrown.</param>
